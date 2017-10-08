fcf601a0e6f93d4a4e29359a84b018fef8694a07
matmcc <matmcconkey@gmail.com>
2017-10-08 12:00:45 +0100

    Changed to Counter

--- ARU_CS_Exercises/Program.cs
+++ ARU_CS_Exercises/Program.cs
@@ -9,11 +9,29 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    /*
     class HelloWorld
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+    */
+
+    class CountUp
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            Console.WriteLine($"Current total is {counter}");
+            counter += 2;
+            Console.WriteLine($"Current total is {counter}");
+            counter += 1;
+            Console.WriteLine($"Current total is {counter}");
+            counter++;
+            Console.WriteLine($"Current total is {counter}");
             Console.ReadLine();
         }
     }
