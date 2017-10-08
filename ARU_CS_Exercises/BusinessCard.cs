055e8ba6e1256dfc400fdaacd29b6b1dec7d00b5
matmcc <matmcconkey@gmail.com>
2017-10-08 12:40:29 +0100

    BusinessCard pseudocode

---
+++ ARU_CS_Exercises/BusinessCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARU_CS_Exercises
+{
+    class BusinessCard
+    {
+        // Get name, number, business
+        // width = Max(Length(name, number, business)) + whitespace_padding
+        // height = array.length(of_above_things) + padding
+        // if topline or bottomline print horizontal line of border char
+        // else print (first char = border char) + (padded input) + (last char = border char)
+    }
+}
